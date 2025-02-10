7777a6489259024919f9c20aa1d2c37270329299
tylerwhitt116 <88746909+tylerwhitt116@users.noreply.github.com>
2025-02-10 18:43:11 -0500

    first commit - working api

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+
+
+const Rappers = {
+    "21 savage":{
+        "age": 29,
+    "birthName":'Jerry' ,
+    "birthLocation":'London, England'
+    },
+
+    "chance the rapper":{
+        "age": 28,
+    "birthName":'Chancelor' ,
+    "birthLocation":'Illinois'
+    },
+    "dylan":{
+        "age": "dylan",
+    "birthName":'dylan' ,
+    "birthLocation":'dylan'
+    }
+}
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/api/:rapperName', (req, res) =>{
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(Rappers[rappersName]){
+        res.json(Rappers[rappersName]
+    )}else{
+        res.json(Rappers['dylan'])
+    }
+})
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('listening on 3000')
+  })
+
